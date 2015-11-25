84633850d36e59247f88ca30d9969d8b5eb1abe4
panjiabang <panjiabang@gmail.com>
2015-11-26 03:48:25 +0800

    try openresty

--- nginx/lua/dynamic-upstream-weight.lua
+++ nginx/lua/dynamic-upstream-weight.lua
@@ -2,93 +2,145 @@ local res_time_dict = ngx.shared.upstream_res_time_dict
 local weight_dict = ngx.shared.upstream_weight_dict
 local upstream = require "ngx.upstream"
 
-local ever_max = res_time_dict:get("max") or 0
-local ever_min = res_time_dict:get("min") or 99999
-local need_update_weight = false
 
-local idx_addr = 1;
-for addr in string.gmatch(ngx.var.upstream_addr, "([^, ]+)") do
-    local idx_time = 1;
-    for time in string.gmatch(ngx.var.upstream_response_time, "([^, ]+)") do
-        if idx_addr == idx_time then
-            local passed_time = res_time_dict:get(addr) or 0
-            local new_time = passed_time * 0.3 + tonumber(time) * 0.7
-            res_time_dict:set(addr, new_time)
-            local diff = math.abs((new_time - passed_time) / passed_time)
-            if diff > 0.2 then
-                need_update_weight = true
-                ngx.log(ngx.ALERT, "need to update upstream weight because diff " .. diff .. " greater than ".. 0.2)
-            end
-            if new_time > ever_max then
-                need_update_weight = true
-                ngx.log(ngx.ALERT, "need to update upstream weight because max value outdated")
-            end
-            if new_time < ever_min then
-                need_update_weight = true
-                ngx.log(ngx.ALERT, "need to update upstream weight because min value outdated")
-            end
-        end
-        idx_time = idx_time + 1
+local string2array = function (str)
+    local arr = {}
+    for field in string.gmatch(str, "([^, ]+)") do
+        table.insert(arr, field)
     end
-    idx_addr = idx_addr + 1;
+    return arr
 end
 
-local srvs, err = upstream.get_primary_peers("backend_blog_jamespan_me")
-if srvs then
-    for _, srv in ipairs(srvs) do
-        local real_weight = srv["weight"]
-        local server_name = srv["name"]
-        local save_weight = weight_dict:get(server_name) or 1
-        if save_weight ~= real_weight then
-            need_update_weight = true
-            ngx.log(ngx.ALERT, "need to update upstream weight because different config found " .. server_name .. "~" .. save_weight .. "~" .. real_weight)
-            break
+
+local update_server_res_time = function (addr, time, dict, passed_weight, current_weight)
+    local passed_time = dict:get(addr) or 0
+    local current_time = tonumber(time)
+    if current_time == nil then
+        current_time = passed_time
+    end
+    local new_time = passed_time * passed_weight + current_time * current_weight
+    dict:set(addr, new_time)
+    return new_time, passed_time
+end
+
+
+--check if we need to update the weights, with some strategy
+local check_if_update_weight = function (addr, new_time, old_time, dict)
+    local diff = math.abs((new_time - old_time) / old_time)
+    local threshold = 0.2
+    if diff > threshold then
+        ngx.log(ngx.ALERT, "need to update upstream weight because diff " .. diff .. " greater than ".. threshold .. " on " .. addr)
+        return true
+    end
+
+    local ever_max = dict:get("max") or 0
+    if new_time > ever_max then
+        ngx.log(ngx.ALERT, "need to update upstream weight because max value outdated")
+        return true
+    end
+
+    local ever_min = dict:get("min") or 99999
+    if new_time < ever_min then
+        ngx.log(ngx.ALERT, "need to update upstream weight because min value outdated")
+        return true
+    end
+
+    return false
+end
+
+local check_if_weight_changed_unexpected = function (ups, upsteam_name, dict)
+    local servers, err = ups.get_primary_peers(upsteam_name)
+    if not servers then
+        ngx.log(ngx.ALERT, "get upstream " .. upsteam_name .. " fail, " .. err)
+        return false
+    end
+    for _, server in ipairs(servers) do
+        local server_name = server["name"]
+        local actual = server["weight"]
+        local expect = dict:get(server_name) or 1
+        if actual ~= expect then
+            ngx.log(ngx.ALERT, "need to update upstream weight because different config found " .. server_name .. "~" .. expect .. "~" .. actual)
+            return true
         end
     end
+    return false
+end
+
+local gen_server_time_map = function(dict)
+    local keys = res_time_dict:get_keys(100)
+    local map = {}
+    for _, key in ipairs(keys) do
+        local time = dict:get(key)
+        map[key] = time
+    end
+    return map
+end
+
+local min_max = function (t)
+    local max = -math.huge
+    local min = math.huge
+    for k, v in pairs(t) do
+        max = math.max(max, v)
+        min = math.min(min, v)
+    end
+    return min, max
+end
+
+local update_server_weight = function (ups, upsteam_name, dict, server_time_map, max_time)
+    local servers, err = ups.get_primary_peers(upsteam_name)
+    if not servers then
+        ngx.log(ngx.ALERT, "get upstream " .. upsteam_name .. " fail, " .. err)
+        return
+    end
+    for _, server in ipairs(servers) do
+        local server_name = server["name"]
+        local id = server["id"]
+        local fails = server["fails"]
+        local time = server_time_map[server_name]
+        local weight = 0
+
+        if fails > 0 then
+            weight = 1
+            ngx.log(ngx.ALERT, "down grade " .. server_name .. " because it fail " .. fails .. " times")
+        elseif time ~= nil and time > 0 then
+            weight = math.ceil(max_time / time)
+        end
+        
+        if weight > 0 then
+            upstream.set_peer_weight("backend_blog_jamespan_me", false, id, weight)
+            upstream.set_peer_effective_weight("backend_blog_jamespan_me", false, id, weight)
+
+            dict:set(server_name, weight)
+        end
+    end
+end
+
+
+local ups_identity = "backend_blog_jamespan_me"
+--it may have multi address and times if nginx tried more than 1 upstream servers
+--but most of the time, there's only one address and time
+local addrs = string2array(ngx.var.upstream_addr)
+local times = string2array(ngx.var.upstream_response_time)
+
+local need_update_weight = false
+for idx, addr in ipairs(addrs) do
+    local time = times[idx]
+    local new_time, old_time = update_server_res_time(addr, time, res_time_dict, 0.3, 0.7)
+    need_update_weight = check_if_update_weight(addr, new_time, old_time, res_time_dict)
+end
+
+if not need_update_weight then
+    need_update_weight = check_if_weight_changed_unexpected(upstream, ups_identity, weight_dict)
 end
 
 if need_update_weight then
-    local keys = res_time_dict:get_keys(10)
-    local total = 0
-    local max_time = 0
-    local min_time = 99999
-    local server_time_dict = {}
-
-    for _, key in pairs(keys) do
-        local time = res_time_dict:get(key)
-        server_time_dict[key] = time
-        total = total + time
-        if time > max_time then
-            max_time = time
-        end
-        if time < min_time then
-            min_time = time
-        end
-    end
+    local server_time_map = gen_server_time_map(res_time_dict)
+    local min_time, max_time = min_max(server_time_map)
 
     res_time_dict:set("max", max_time)
     res_time_dict:set("min", min_time)
 
-    local srvs, err = upstream.get_primary_peers("backend_blog_jamespan_me")
-    if srvs then
-        for _, srv in ipairs(srvs) do
-            local server_name = srv["name"]
-            local server_res_time = server_time_dict[server_name]
-            if server_res_time ~= nil then
-                local weight = math.ceil(max_time / server_res_time)
-                --weight = math.pow(2, weight)
-                local fails = srv["fails"]
-                if fails > 0 then
-                    weight = 1
-                end
-                local id = srv["id"]
-                upstream.set_peer_weight("backend_blog_jamespan_me", false, id, weight)
-                weight_dict:set(server_name, weight)
-                --upstream.set_peer_current_weight("backend_blog_jamespan_me", false, id, 0)
-                upstream.set_peer_effective_weight("backend_blog_jamespan_me", false, id, weight)
-            end
-        end
-    end
+    update_server_weight(upstream, ups_identity, weight_dict, server_time_map, max_time)
 end
 
 
